055c6515911a6344c8bbd4555780f6b1e8aea343
hiab gebremichael <hiab.gebremichael.stu.ocsb.ca>
2025-05-05 17:09:52 +0000

    temp_convert.py

--- temp_convert.py
+++ temp_convert.py
@@ -6,29 +6,29 @@
 def convert_celsius():
 
     try:
-    # Input
-        celsius = float(input("Enter celsius that you would like to convert to fahrenheit :"))
-    # Process to convert celsius to fahrenheit
-        fahrenheit = celsius * 9/5 + 32
-    # Output 
-        print(f"{celsius} in fahrenheit is {fahrenheit} ". format(fahrenheit))
+        # Input
+        celsius = float(
+            input("Enter celsius that you would like to convert to fahrenheit :")
+        )
+        # Process to convert celsius to fahrenheit
+        fahrenheit = celsius * 9 / 5 + 32
+        # Output
+        print(f"{celsius} in fahrenheit is {fahrenheit} ".format(fahrenheit))
 
-# Catch erroneous input
+    # Catch erroneous input
     except Exception:
 
         print("Please enter a valid input.")
 
+
 if __name__ == "__main__":
     convert_celsius()
+
 
 def main():
     # Call function
     convert_celsius()
 
+
 if __name__ == "__main__":
     main()
-
-
-
-
-
